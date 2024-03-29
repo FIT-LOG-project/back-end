055a6a22a3e2a8eb5700275c1a74a54cde514fc9
sangwoo <lesw1216@gmail.com>
2024-03-29 17:35:33 +0900

    setting: Response 성공, 실패 포맷 클래스 구성
    1. Response의 구조를 동일 하게 내려주기 위해 포맷 클래스를 생성하였다.

---
+++ src/main/java/com/swoo/fitlog/http/ErrorResponse.java
@@ -0,0 +1,26 @@
+package com.swoo.fitlog.http;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Set;
+
+@Getter
+@Builder
+public class ErrorResponse {
+    private int code;
+    private HttpStatus httpStatus;
+    private String message;
+    private Set<Integer> errorCodes;
+
+    public static ErrorResponse of(int code, HttpStatus httpStatus, String message, Set<Integer> errorCodes) {
+        return ErrorResponse.builder()
+                .code(code)
+                .httpStatus(httpStatus)
+                .message(message)
+                .errorCodes(errorCodes)
+                .build();
+    }
+}
+
