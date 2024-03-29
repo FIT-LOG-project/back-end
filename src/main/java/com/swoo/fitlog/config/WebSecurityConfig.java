87b508dee7584f8b569efe6f644a73c4175a0c58
sangwoo <lesw1216@gmail.com>
2024-03-29 21:37:55 +0900

    feat: 이메일 인증 번호 전송
    회원 가입 시 입력한 이메일에 인증 번호를 전송하는 feat

--- src/main/java/com/swoo/fitlog/config/WebSecurityConfig.java
+++ src/main/java/com/swoo/fitlog/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.swoo.fitlog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize ->
                 authorize
